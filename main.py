69f52007df0d277512a5b2b0e5a6d385888d2fb4
Excel <excelthebeanman@gmail.com>
2023-05-19 09:18:37 +0200

    implemented slash commands

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
+import binarytree
 import discord
 from discord.ext import commands
-from discord import app_commands
+from discord import app_commands, Interaction
 from dotenv import load_dotenv
 import os
 
@@ -11,6 +12,7 @@ import os
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_PREFIX = os.getenv("PREFIX")
+DISCORD_DEV_ID = int(os.getenv('DISCORD_DEV_ID'))
 
 class Hivemaster(commands.Bot):
     
@@ -24,17 +26,34 @@ class Hivemaster(commands.Bot):
             status=discord.Status.online
         )
         
-
-        
 async def main():
-    h = Hivemaster()
+    bot = Hivemaster()
     
-    @h.event
+    # Slash command syncing
+    # ======================================================
+    
+    @bot.command()
+    async def localsync(ctx: commands.Context):
+        if ctx.author.id != DISCORD_DEV_ID:
+            await ctx.reply("`Command failed: missing permission`")
+            return
+        bot.tree.copy_global_to(guild=ctx.guild)
+        await bot.tree.sync(guild=ctx.guild)
+        await ctx.reply("Commands synced locally!")
+        
+    # The Hive Game
+    # ======================================================
+        
+    @bot.tree.command(name="start-game", description="Start a new game of The Hive.")
+    async def start_game(interaction: Interaction):
+        await interaction.response.send_message("New game started!")
+    
+    @bot.event
     async def on_ready():
-        print(f'Logged into Discord as {h.user}.')
+        print(f'Logged into Discord as {bot.user} with prefix {DISCORD_PREFIX}')
     
     print("Starting Hivemaster Discord Bot...")
-    async with h:
-        await h.start(DISCORD_TOKEN)
+    async with bot:
+        await bot.start(DISCORD_TOKEN)
     
 asyncio.run(main())
